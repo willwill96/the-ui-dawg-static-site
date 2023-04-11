f4ccfa10d8134ca90d8405b7d09dd3a8e9f95c28
William Will <10997562+willwill96@users.noreply.github.com>
2023-04-11 23:41:59 +0000

    fix: base urls for images

--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -1,8 +1,9 @@
 export const convertToBaseUrl = (value: string) => {
-  if (import.meta.env.BASE_URL && import.meta.env.BASE_URL !== "/") {
-    return `/${value}`;
+  if (import.meta.env.BASE_URL) {
+    return `${import.meta.env.BASE_URL}/${value}`;
   }
-  return value;
+  return `/${value}`;
+
 };
 
 export const removeBaseUrl = (value: string) => {
